3917411ccfdad56195e5607b6058d4915bca886d
Bryan Saunders <btsaunde@gmail.com>
2015-03-21 10:06:38 -0400

    Fixing JS Errors

--- src/main/webapp/app/randomize/randomize.controller.js
+++ src/main/webapp/app/randomize/randomize.controller.js
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -37,7 +37,7 @@
 
         function activate() {
           vm.schemes={};
-          vm.masterMinds={};
+          vm.masterminds={};
 
           randomizeService.getSchemes()
           .success(getSchemesSuccess)
@@ -45,7 +45,7 @@
 
           randomizeService.getMasterminds()
           .success(getMastermindSuccess)
-          .error(getMasterMindError);
+          .error(getMastermindError);
         }
 
         function getSchemesSuccess(schemes){
@@ -55,10 +55,10 @@
           alert("Errors");
         }
 
-        function getSchemesSuccess(masterminds){
+        function getMastermindSuccess(masterminds){
           vm.masterminds = masterminds;
         }
-        function getSchemesError(data){
+        function getMastermindError(data){
           alert("Errors");
         }
     }
