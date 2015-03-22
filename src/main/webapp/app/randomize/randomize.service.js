b2fca77b2322be4a3a35d7101f355c57f00def0c
Crystal <cdsmallw@gmail.com>
2015-03-21 23:07:10 -0400

    Updating the http endpoint and cleaning up the presentation of the setup

--- src/main/webapp/app/randomize/randomize.service.js
+++ src/main/webapp/app/randomize/randomize.service.js
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -34,15 +34,15 @@ function randomizeService($http) {
   return service;
 
   function getSchemes(){
-    var url = 'http://legendary-bsaunders.rhcloud.com/rest/scheme';
+    var url = 'http://bts-fsw.airdns.org:30454/rest/scheme';
     return $http.get(url);
   }
   function getMasterminds(){
-    var url = 'http://legendary-bsaunders.rhcloud.com/rest/mastermind';
+    var url = 'http://bts-fsw.airdns.org:30454/rest/mastermind';
     return $http.get(url);
   }
   function getSetup(setup){
-    var url = 'http://legendary-bsaunders.rhcloud.com/rest/setup/{0}';//"?scheme={1}&mastermind={2}";
+    var url = 'http://bts-fsw.airdns.org:30454/rest/setup/{0}';//"?scheme={1}&mastermind={2}";
     url = url.replace('{0}', setup.numOfPlayers);
     if(setup.selectedMastermind || setup.selectedScheme){
       url = url + '?';
